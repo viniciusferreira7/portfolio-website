fa1e03a12a803e72926fca107a4b71c2e0dcd24b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-09-04 17:00:55 -0300

    style(assets index.html): add fonts and images

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,6 +1,6 @@
-import Container from 'react-bootstrap/Container'
-import Nav from 'react-bootstrap/Nav'
-import Navbar from 'react-bootstrap/Navbar'
+import { MouseEventHandler, useEffect, useState } from 'react'
+
+import { Container, Nav, Navbar } from 'react-bootstrap'
 
 import {
   AiFillGithub,
@@ -10,8 +10,35 @@ import {
 } from 'react-icons/ai'
 
 export function NavBar() {
+  const [activeLink, setActiveLink] = useState('home')
+  const [scrolled, setScrolled] = useState(false)
+
+  useEffect(() => {
+    function onScroll() {
+      if (window.screenY > 50) {
+        setScrolled(true)
+      } else {
+        setScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
+
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [])
+
+  function handleUpdateActiveLink(
+    linkName: string,
+  ): MouseEventHandler<HTMLElement> | undefined {
+    setActiveLink(linkName)
+
+    return undefined
+  }
+
   return (
-    <Navbar expand="lg">
+    <Navbar expand="lg" className={scrolled ? 'scrolled' : ''}>
       <Container>
         <Navbar.Brand href="#home">
           <img src="" alt="Logo" />
@@ -21,9 +48,33 @@ export function NavBar() {
         </Navbar.Toggle>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#skills">Skills</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link
+              href="#home"
+              className={
+                activeLink === 'home' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={handleUpdateActiveLink('home')}
+            >
+              Home
+            </Nav.Link>
+            <Nav.Link
+              href="#skills"
+              className={
+                activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={handleUpdateActiveLink('skills')}
+            >
+              Skills
+            </Nav.Link>
+            <Nav.Link
+              href="#projects"
+              className={
+                activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'
+              }
+              onClick={handleUpdateActiveLink('projects')}
+            >
+              Projects
+            </Nav.Link>
           </Nav>
           <span className="navbar-text">
             <div className="social-icon">
