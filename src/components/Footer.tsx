776e1d163ca94a6de2b717d71dc714d86fe69c2d
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-09-05 23:23:00 -0300

    style(footer): centering the logo

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -15,7 +15,7 @@ export function Footer() {
       <Container>
         <Row className="align-items-center">
           <MailchimpForm />
-          <Col size={12} sm={6}>
+          <Col size={12} sm={6} className="text-center text-sm-start">
             <img src={logo} alt="Logo" />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
